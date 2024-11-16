7ccb2219611f4cbb885882cdc8544ed27ee8223b
Dominik Sobolewski <61312276+dsbbl@users.noreply.github.com>
2024-11-16 23:16:26 +0100

    404 Not found handling (#15)

--- client/src/components/NotFound/NotFound.tsx
+++ client/src/components/NotFound/NotFound.tsx
@@ -1,11 +1,17 @@
 import {Link} from 'react-router-dom';
+import {Image} from 'react-bootstrap';
+import notFound from '../../assets/notfound.png';
+import './NotFound.scss';
 
 const NotFound = () => {
   return (
-    <div style={{textAlign: 'center', padding: '50px'}}>
-      <h1>404 - Strona nie znaleziona</h1>
-      <p>Przepraszamy, ale ta strona nie istnieje.</p>
-      <Link to='/'>Powrót na stronę główną</Link>
+    <div className='not-found'>
+      <div className='not-found body'>
+        <Image src={notFound} rounded />      
+        <h1>404 - Strona nie znaleziona</h1>
+        <p>Przepraszamy, ale ta strona nie istnieje.</p>
+        <Link to='/'>Powrót na stronę główną</Link>
+      </div>
     </div>
   );
 };
