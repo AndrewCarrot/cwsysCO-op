34f38f2a11ae0d07cba8b8dcbf5a55f17a682096
dsbbl <domin25001@gmail.com>
2024-11-22 18:52:01 +0100

    Fix after login redirect

--- client/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,8 +1,19 @@
 import {Navigate} from 'react-router-dom';
 import {useTokenValidation} from '../../hooks/useTokenValidation';
+import Spinner from 'react-bootstrap/Spinner';
 
 const ProtectedRoute = ({children}: { children: JSX.Element }) => {
   const [isTokenValid] = useTokenValidation();
+
+  if (isTokenValid === null) {
+    return (
+      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh'}}>
+        <Spinner animation='border' role='status'>
+          <span className='visually-hidden'>Loading...</span>
+        </Spinner>
+      </div>
+    );
+  }
 
   if (!isTokenValid) {
     sessionStorage.removeItem('token');
